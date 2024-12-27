28733d347fbfa767000dc0fb16945fd4abe49ad1
Jean-Elie Pierre <gno320@gmail.com>
2024-12-27 01:39:38 -0500

    container now runs

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
 # use --no-cache to build from scratch
-docker build -t cuas:1.0 -f docker\Dockerfile .
+docker build -t vs-code-docker-gui:1.0 -f docker\Dockerfile .
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 Xvfb -screen 0 1024x768x24 -ac &
 sleep 5
-# export DISPLAY=host.docker.internal:0.0
-# x11vnc -rfbauth ~/.vnc/passwd -noxrecord -noxfixes -noxdamage -forever -display host.docker.internal:0.0 -bg
 export DISPLAY=:0.0
-x11vnc -rfbauth ~/.vnc/passwd -noxrecord -noxfixes -noxdamage -forever -display :0.0 -bg
+x11vnc -rfbauth ~/.vnc/passwd -noxrecord -noxfixes -noxdamage -forever -display :0 -bg
 xfce4-session &
 
-[ -f "/app.sh" ] && sh /app.sh || /bin/bash
+# [ -f "/app.sh" ] && sh /app.sh || /bin/bash
