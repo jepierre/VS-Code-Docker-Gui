a5388f839348238c8403aa15b67b9d5eaabd3117
Jean-Elie Pierre <gno320@gmail.com>
2024-12-27 02:34:20 -0500

    update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,1 @@
-set-variable -name DISPLAY -value 192.168.1.29:0.0
-# docker run --rm -it -v ${pwd}:/workspace/cuas -p5900:5900 -e DISPLAY=$DISPLAY --gpus all  rayproject/ray-ml:latest-gpu
-docker run --rm -it -v ${pwd}:/home/dev/workspace/cuas -p5900:5900 -e DISPLAY=$DISPLAY --gpus all  cuas:1.0 bash
+docker run --rm -it -v ${pwd}:/home/dev/workspace -p5900:5900 -e DISPLAY=$DISPLAY --gpus all  cuas:1.0 bash
